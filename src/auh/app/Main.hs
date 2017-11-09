84adff0fc0655f65aeea726485590b3219155b33
ashic <ashic@live.com>
2017-11-09 08:45:10 +0000

    added an api

--- src/auh/app/Main.hs
+++ src/auh/app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Model
+import Data.String
+import Web.Scotty
+
+routes = do
+
+    get "/hello" $ text "hello"
+
+    get "/hello/:name" $ do
+        name <- param "name"
+        text $ fromString $ "Hello " ++ name
 
 main :: IO ()
-main = someFunc
+main = scotty 3000 routes
